e4aa259de7adb771cac1171ec9eaddf401821656
Thomas Bailey <thomasrobertbailey@gmail.com>
2016-09-09 16:36:33 -0700

    Added human-readable report for check-in.

--- checkIn.sh
+++ checkIn.sh
@@ -2,7 +2,16 @@ echo "Pushing local changes to server..."
 
 hg addremove overlays sprites objects animations transitions
 
+echo ""
+echo "Full diff:"
 hg diff --stat
+
+echo ""
+./checkInReport.sh
+
+echo ""
+
+
 
 echo "Enter commit message."
 echo -n "> "
