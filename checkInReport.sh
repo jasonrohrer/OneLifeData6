26efeacb07978cb3b52f083d22b6d145a0094368
Thomas Bailey <thomasrobertbailey@gmail.com>
2016-09-19 11:04:17 -0700

    Pine doors and walls finished.  Removed all test walls.

--- checkInReport.sh
+++ checkInReport.sh
@@ -8,8 +8,13 @@ f=$(echo $x | sed 's/\s.*$//');
 
 if [[ $f == sprites/*.txt ]] && ! [[ $f == sprites/nextSpriteNumber.txt ]];
 then
-name=$(cat $f | sed 's/\s.*$//');
-echo "  \"$name\""; 
+if [ -e $f ]
+then
+  name=$(cat $f | sed 's/\s.*$//');
+  echo "  \"$name\""; 
+else
+  echo "$f removed"
+fi
 fi
 
 done
@@ -25,8 +30,13 @@ f=$(echo $x | sed 's/\s.*$//');
 
 if [[ $f == objects/*.txt ]] && ! [[ $f == objects/nextObjectNumber.txt ]];
 then
-name=$(cat $f | sed -n 2p );
-echo "  \"$name\""; 
+if [ -e $f ]
+then
+  name=$(cat $f | sed -n 2p );
+  echo "  \"$name\""; 
+else
+  echo "$f removed"
+fi
 fi
 
 done
@@ -44,12 +54,17 @@ f=$(echo $x | sed 's/\s.*$//');
 if [[ $f == animations/*.txt ]];
 then
 
-id=$(echo "$f" | sed 's/.*\///' | sed 's/_.*//' );
-anim=$(echo "$f" | sed 's/.*\///' | sed 's/\..*//' | sed 's/.*_//' );
+if [ -e $f ]
+then
+  id=$(echo "$f" | sed 's/.*\///' | sed 's/_.*//' );
+  anim=$(echo "$f" | sed 's/.*\///' | sed 's/\..*//' | sed 's/.*_//' );
 
-oName=$(cat "objects/$id.txt" | sed -n 2p );
+  oName=$(cat "objects/$id.txt" | sed -n 2p );
 
-echo "  \"$oName\"   $anim"; 
+  echo "  \"$oName\"   $anim";
+else 
+  echo "$f removed"
+fi
 fi
 
 done
@@ -66,67 +81,72 @@ f=$(echo $x | sed 's/\s.*$//');
 if [[ $f == transitions/*.txt ]];
 then
 
-actorID=$(echo "$f" | sed 's/.*\///' | sed 's/_.*//' );
-targetID=$(echo "$f" | sed 's/.*\///' | sed 's/\..*//' | sed 's/.*_//' );
-
-newActorID=$(cat $f | sed 's/\s.*//' );
-newTargetID=$(cat $f | sed 's/[^ ]* //' | sed 's/\s.*//' );
-decayTime=$(cat $f | sed 's/[^ ]* //' | sed 's/.*\s//' );
-
-
-decayString=""
-actor="";
-if [[ $actorID == -1 ]];
+if [ -e $f ]
 then
+  actorID=$(echo "$f" | sed 's/.*\///' | sed 's/_.*//' );
+  targetID=$(echo "$f" | sed 's/.*\///' | sed 's/\..*//' | sed 's/.*_//' );
+
+  newActorID=$(cat $f | sed 's/\s.*//' );
+  newTargetID=$(cat $f | sed 's/[^ ]* //' | sed 's/\s.*//' );
+  decayTime=$(cat $f | sed 's/[^ ]* //' | sed 's/.*\s//' );
+
+
+  decayString=""
+  actor="";
+  if [[ $actorID == -1 ]];
+  then
 	actor="[DECAY]"
     decayString="($decayTime seconds)"
-elif [[ $actorID == 0 ]];
-then
+  elif [[ $actorID == 0 ]];
+  then
 	actor="[HAND]"
-elif [[ $actorID == -2 ]];
-then
+  elif [[ $actorID == -2 ]];
+  then
 	actor="[DEFAULT]"
-else
+  else
 	actor=$(cat "objects/$actorID.txt" | sed -n 2p );
 	actor="\"$actor\"";
-fi
+  fi
 
 
-target="";
-if [[ $targetID == -1 ]] && [[ $newTargetID == 0 ]];
-then
+  target="";
+  if [[ $targetID == -1 ]] && [[ $newTargetID == 0 ]];
+  then
 	target="[EAT]"
-elif [[ $targetID == -1 ]] && [[ $newTargetID != 0 ]];
-then
+  elif [[ $targetID == -1 ]] && [[ $newTargetID != 0 ]];
+  then
 	target="[BARE-GROUND]"
-elif [[ $targetID == 0 ]];
-then
+  elif [[ $targetID == 0 ]];
+  then
 	target="[ON-PERSON]"
-else
+  else
 	target=$(cat "objects/$targetID.txt" | sed -n 2p );
 	target="\"$target\"";
-fi
+  fi
 
-newActor=""
-if [[ $newActorID == 0 ]];
-then
+  newActor=""
+  if [[ $newActorID == 0 ]];
+  then
 	newActor="[NOTHING]"
-else
+  else
 	newActor=$(cat "objects/$newActorID.txt" | sed -n 2p );
 	newActor="\"$newActor\"";
-fi
+  fi
 
-newTarget=""
-if [[ $newTargetID == 0 ]];
-then
+  newTarget=""
+  if [[ $newTargetID == 0 ]];
+  then
 	newTarget="[NOTHING]"
-else
+  else
 	newTarget=$(cat "objects/$newTargetID.txt" | sed -n 2p );
 	newTarget="\"$newTarget\"";
+  fi
+
+
+  echo "  $actor  +  $target   =   $newActor  +  $newTarget  $decayString"; 
+else 
+  echo "$f removed"
 fi
-
-
-echo "  $actor  +  $target   =   $newActor  +  $newTarget  $decayString"; 
 fi
 
 done
