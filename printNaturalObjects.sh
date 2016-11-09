42d90fbfcdd7785d041cb139d6572ebf95f2e557
Thomas Bailey <thomasrobertbailey@gmail.com>
2016-11-09 13:07:00 -0800

    Included biomes in map chance report.

--- printNaturalObjects.sh
+++ printNaturalObjects.sh
@@ -14,10 +14,11 @@ if [[ $line == mapChance* ]];
 then
 
 chance=$(echo $line | grep -o -E '[0-9.]+' | head -1 );
+biomes=$(echo $line | grep -o -E 'biomes_[0-9,]+' | tail -1 );
 
 if (( $(bc <<< "$chance > 0") ))
 then
-echo "$chance  $name" 
+echo -e "$chance  $biomes \t $name" 
 fi
 
 break
